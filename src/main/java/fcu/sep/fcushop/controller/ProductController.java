311a5eb0034c10579c0f1c167f8c73e9ad8b8ade
D0886274 <D0886274@o365.fcu.edu.tw>
2021-12-11 02:54:09 +0800

    first commit

---
+++ src/main/java/fcu/sep/fcushop/controller/ProductController.java
@@ -0,0 +1,50 @@
+package fcu.sep.fcushop.controller;
+
+import fcu.sep.fcushop.model.Product;
+import fcu.sep.fcushop.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 点对 (x,y) 的水平和垂直距离.
+ */
+
+@RestController
+public class ProductController {
+
+  @Autowired
+  ProductService productManager;
+
+  @GetMapping("/products")
+  public List<Product> getProducts() {
+    return productManager.getProducts();
+  }
+
+  @GetMapping("/products/{keyword}")
+  public List<Product> getProducts(@PathVariable("keyword") String keyword) {
+    return productManager.getProducts(keyword);
+  }
+
+  @RequestMapping(value="/add_product", method = RequestMethod.GET)
+  @ResponseBody
+  public String addProduct(
+      @RequestParam("game_name") String game_name,
+      @RequestParam("img_url") String img_url,
+      @RequestParam("price") Integer price,
+      @RequestParam("description") String description
+  )
+  {
+    return productManager.AddProduct(game_name, img_url, price, description);
+  }
+
+  @RequestMapping(value="/update_product", method = RequestMethod.GET)
+  @ResponseBody
+  public String updateProduct(
+      @RequestParam("game_name") String game_name,
+      @RequestParam("price") Integer price
+  )
+  {
+    return productManager.UpdateProduct(game_name, price);
+  }
+}
---
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -0,0 +1,88 @@
+package fcu.sep.fcushop.service;
+
+import fcu.sep.fcushop.database.Sql2oDbHandler;
+import fcu.sep.fcushop.model.Product;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.sql2o.Connection;
+/**
+ * 点对 (x,y) 的水平和垂直距离.
+ */
+
+
+@Service
+public class ProductService {
+
+  @Autowired
+  private Sql2oDbHandler sql2oDbHandler;
+
+  public ProductService() {
+
+  }
+  /**
+   * 点对 (x,y) 的水平和垂直距离.
+   */
+
+  public List<Product> getProducts() {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select ID id,NAME name,IMAGE_URL imageUrl,PRICE price,DESCRIPTION description"
+          + " from PRODUCT";
+
+      return connection.createQuery(query).executeAndFetch(Product.class);
+    }
+  }
+
+
+  /**
+   * 点对 (x,y) 的水平和垂直距离.
+   */
+
+  public List<Product> getProducts(String keyword) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+          + " from PRODUCT where name like :keyword";
+
+      return connection.createQuery(query)
+          .addParameter("keyword", "%"+keyword+"%")
+          .executeAndFetch(Product.class);
+    }
+  }
+
+  public String AddProduct(String game_name, String img_url, int price, String description)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "insert into PRODUCT (NAME, IMAGE_URL, PRICE, DESCRIPTION) "
+          + "VALUES(:game_name, :img_url, :price, :description)";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("game_name", game_name)
+          .addParameter("img_url", img_url)
+          .addParameter("price", price)
+          .addParameter("description", description)
+          .executeUpdate();
+
+      return "Success";
+
+    }
+  }
+
+  public String UpdateProduct(String game_name, int price)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "Update PRODUCT "
+          + "SET PRICE= :price WHERE NAME = :game_name";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("game_name", game_name)
+          .addParameter("price", price)
+          .executeUpdate();
+      return "Success";
+
+    }
+  }
+
+}
