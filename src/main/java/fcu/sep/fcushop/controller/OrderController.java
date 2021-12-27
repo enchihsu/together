a8fa71378f1ddf134cb611fe27f560e01b910df4
D0843969 <wen3131770@gmail.com>
2021-12-28 02:26:43 +0800

    加入購物車1

--- src/main/java/fcu/sep/fcushop/controller/OrderController.java
+++ src/main/java/fcu/sep/fcushop/controller/OrderController.java
@@ -1,6 +1,7 @@
 package fcu.sep.fcushop.controller;
 
 import fcu.sep.fcushop.model.Order;
+import fcu.sep.fcushop.model.Product;
 import fcu.sep.fcushop.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,24 @@ public class OrderController {
   @GetMapping("/orders")
   public List<Order> getOrders() {
     return orderManager.getOrders();
-
   }
+
+  @GetMapping("/addcart/{book}")
+  public String addcart(@PathVariable("book") String book) {
+    return orderManager.addcart(book);
+  }
+
+ /* @RequestMapping(value="/addcart", method = RequestMethod.GET)
+  @ResponseBody
+  public String addProduct(
+      @RequestParam("book_name") String book_name
+      //@RequestParam("img_url") String img_url,
+      //@RequestParam("price") Integer price,
+      //@RequestParam("quantity") Integer quantity
+      //@RequestParam("description") String description
+  )
+  {
+    return orderManager.addCart(book_name);
+  }*/
+
 }
