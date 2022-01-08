280b3ef55c9e53da8288122ab418c0fcea0ab5ac
enchihsu <D0886274@o365.fcu.edu.tw>
2022-01-08 23:10:36 +0800

    shopping cart 傳帳號

--- src/main/java/fcu/sep/fcushop/service/OrderService.java
+++ src/main/java/fcu/sep/fcushop/service/OrderService.java
@@ -26,64 +26,72 @@ public class OrderService {
    * 点对 (x,y) 的水平和垂直距离.
    */
 
-  public List<Order> getOrders() {
+  public List<Order> getOrders(String account) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select product.NAME book,product.IMAGE_URL imageUrl,product.PRICE price,order1.amount amount from bookstore.product inner join bookstore.order1 on product.ID=order1.BOOK";
-      return connection.createQuery(query).executeAndFetch(Order.class);
+      String query = "select product.NAME book,product.IMAGE_URL imageUrl,product.PRICE price,order1.amount amount " +
+          "from bookstore.product inner join bookstore.order1 on product.ID=order1.BOOK where ACCOUNT = :account";
+      System.out.println(query);
+      return connection.createQuery(query).addParameter("account", account).executeAndFetch(Order.class);
     }
   }
 
-  public String addcart(String book) {
+  public String addcart(String book,String account) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query1 = "select count(BOOK) from bookstore.order1 where (ACCOUNT = 0912345678 and BOOK = :bookname)";
-      var result= connection.createQuery(query1).addParameter("bookname", book).executeScalar(Integer.class);
+      String query1 = "select count(BOOK) from bookstore.order1 where (ACCOUNT = :account and BOOK = :bookname)";
+      var result= connection.createQuery(query1)
+          .addParameter("bookname", book)
+          .addParameter("account", account)
+          .executeScalar(Integer.class);
       if(result==0){
-        String query = "insert into bookstore.order1 (BOOK, ACCOUNT, AMOUNT) VALUES(:bookname, 0912345678, 1)";
+        String query = "insert into bookstore.order1 (BOOK, ACCOUNT, AMOUNT) VALUES(:bookname,:account, 1)";
 
         System.out.println(query);
         connection.createQuery(query)
             .addParameter("bookname", book)
-            //.addParameter("quantity", quantity) 人
+            .addParameter("account", account)
             .executeUpdate();
       }
       else{
-        String query ="Update bookstore.order1 SET AMOUNT = order1.AMOUNT+1  WHERE (BOOK = :bookname and ACCOUNT=0912345678)";
+        String query ="Update bookstore.order1 SET AMOUNT = order1.AMOUNT+1  WHERE (BOOK = :bookname and ACCOUNT=:account)";
 
         System.out.println(query);
         connection.createQuery(query)
             .addParameter("bookname", book)
-            //.addParameter("quantity", quantity) 人
+            .addParameter("account", account)
             .executeUpdate();
       }
     }
     return "Success";
   }
 
-  public String deleteproduct(String bookname){
+  public String deleteproduct(String book,String account){
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "delete FROM bookstore.order1 where ID=:bookname";
+      String query = "delete FROM bookstore.order1 where (ID=:book and ACCOUNT=:account)";
       connection.createQuery(query)
-          .addParameter("bookname", bookname)
+          .addParameter("book", book)
+          .addParameter("account", account)
           .executeUpdate();
       return "Success";
     }
   }
 
-  public String plus(String id){
+  public String plus(String id,String account){
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "UPDATE bookstore.order1 SET AMOUNT=AMOUNT+1 WHERE ID=:id";
+      String query = "UPDATE bookstore.order1 SET AMOUNT=AMOUNT+1 WHERE (ID=:id and ACCOUNT=:account)";
       connection.createQuery(query)
           .addParameter("id", id)
+          .addParameter("account", account)
           .executeUpdate();
       return "Success";
     }
   }
 
-  public String minus(String id){
+  public String minus(String id,String account){
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "UPDATE bookstore.order1 SET AMOUNT=AMOUNT-1 WHERE ID=:id";
+      String query = "UPDATE bookstore.order1 SET AMOUNT=AMOUNT-1 WHERE (ID=:id and ACCOUNT=:account)";
       connection.createQuery(query)
           .addParameter("id", id)
+          .addParameter("account", account)
           .executeUpdate();
       return "Success";
     }
